7ac2d5221359aef9104cbedd68b99adadfe37d5c
wangzhen <wangzhen-cto@boe.com.cn>
2022-01-07 09:05:09 +0800

    train test

--- pytorch_classification/Test3_vggnet/train.py
+++ pytorch_classification/Test3_vggnet/train.py
@@ -1,4 +1,4 @@
-import os
+import os, time
 import json
 
 import torch
@@ -11,7 +11,7 @@ from model import vgg
 
 
 def main():
-    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    device = torch.device("cuda:1" if torch.cuda.is_available() else "cpu")
     print("using {} device.".format(device))
 
     data_transform = {
@@ -62,17 +62,19 @@ def main():
     net = vgg(model_name=model_name, num_classes=5, init_weights=True)
     net.to(device)
     loss_function = nn.CrossEntropyLoss()
-    optimizer = optim.Adam(net.parameters(), lr=0.0001)
+    optimizer = optim.Adam(net.parameters(), lr=1e-4)
 
-    epochs = 30
+    epochs = 50
     best_acc = 0.0
     save_path = './{}Net.pth'.format(model_name)
     train_steps = len(train_loader)
+    start_time = time.time()
     for epoch in range(epochs):
         # train
         net.train()
         running_loss = 0.0
-        train_bar = tqdm(train_loader)
+        # train_bar = tqdm(train_loader)
+        train_bar = train_loader
         for step, data in enumerate(train_bar):
             images, labels = data
             optimizer.zero_grad()
@@ -84,15 +86,17 @@ def main():
             # print statistics
             running_loss += loss.item()
 
-            train_bar.desc = "train epoch[{}/{}] loss:{:.3f}".format(epoch + 1,
-                                                                     epochs,
-                                                                     loss)
-
+            # train_bar.desc = "train epoch[{}/{}] loss:{:.3f}".format(epoch + 1,
+            #                                                          epochs,
+            #                                                          loss)
+            if step % 50 == 0:
+                print('epoch : {}, idx: {}, loss: {}'.format(epoch, step, loss))
         # validate
         net.eval()
         acc = 0.0  # accumulate accurate number / epoch
         with torch.no_grad():
-            val_bar = tqdm(validate_loader)
+            # val_bar = tqdm(validate_loader)
+            val_bar = validate_loader
             for val_data in val_bar:
                 val_images, val_labels = val_data
                 outputs = net(val_images.to(device))
@@ -100,14 +104,15 @@ def main():
                 acc += torch.eq(predict_y, val_labels.to(device)).sum().item()
 
         val_accurate = acc / val_num
-        print('[epoch %d] train_loss: %.3f  val_accuracy: %.3f' %
+        print('---------------------------[epoch %d] train_loss: %.3f  val_accuracy: %.3f' %
               (epoch + 1, running_loss / train_steps, val_accurate))
 
         if val_accurate > best_acc:
             best_acc = val_accurate
             torch.save(net.state_dict(), save_path)
 
-    print('Finished Training')
+    end_time = time.time()
+    print(f'Finished Training, use {(end_time-start_time)/60} min')
 
 
 if __name__ == '__main__':
--- pytorch_classification/Test4_googlenet/train.py
+++ pytorch_classification/Test4_googlenet/train.py
@@ -6,12 +6,13 @@ import torch.nn as nn
 from torchvision import transforms, datasets
 import torch.optim as optim
 from tqdm import tqdm
+import time
 
 from model import GoogLeNet
 
 
 def main():
-    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    device = torch.device("cuda:1" if torch.cuda.is_available() else "cpu")
     print("using {} device.".format(device))
 
     data_transform = {
@@ -38,7 +39,7 @@ def main():
     with open('class_indices.json', 'w') as json_file:
         json_file.write(json_str)
 
-    batch_size = 32
+    batch_size = 32 
     nw = min([os.cpu_count(), batch_size if batch_size > 1 else 0, 8])  # number of workers
     print('Using {} dataloader workers every process'.format(nw))
 
@@ -73,15 +74,17 @@ def main():
     loss_function = nn.CrossEntropyLoss()
     optimizer = optim.Adam(net.parameters(), lr=0.0003)
 
-    epochs = 30
+    epochs = 70
     best_acc = 0.0
     save_path = './googleNet.pth'
     train_steps = len(train_loader)
+    start_time = time.time()
     for epoch in range(epochs):
         # train
         net.train()
         running_loss = 0.0
-        train_bar = tqdm(train_loader)
+        # train_bar = tqdm(train_loader)
+        train_bar = train_loader
         for step, data in enumerate(train_bar):
             images, labels = data
             optimizer.zero_grad()
@@ -96,15 +99,18 @@ def main():
             # print statistics
             running_loss += loss.item()
 
-            train_bar.desc = "train epoch[{}/{}] loss:{:.3f}".format(epoch + 1,
-                                                                     epochs,
-                                                                     loss)
+            # train_bar.desc = "train epoch[{}/{}] loss:{:.3f}".format(epoch + 1,
+            #                                                          epochs,
+            #                                                          loss)
+            if step % 50 == 0:
+                print('epoch : {}, idx: {}, loss: {}'.format(epoch, step, loss))
 
         # validate
         net.eval()
         acc = 0.0  # accumulate accurate number / epoch
         with torch.no_grad():
-            val_bar = tqdm(validate_loader)
+            # val_bar = tqdm(validate_loader)
+            val_bar = validate_loader
             for val_data in val_bar:
                 val_images, val_labels = val_data
                 outputs = net(val_images.to(device))  # eval model only have last output layer
@@ -112,14 +118,15 @@ def main():
                 acc += torch.eq(predict_y, val_labels.to(device)).sum().item()
 
         val_accurate = acc / val_num
-        print('[epoch %d] train_loss: %.3f  val_accuracy: %.3f' %
+        print('---------------------------[epoch %d] train_loss: %.3f  val_accuracy: %.3f' %
               (epoch + 1, running_loss / train_steps, val_accurate))
 
         if val_accurate > best_acc:
             best_acc = val_accurate
             torch.save(net.state_dict(), save_path)
 
-    print('Finished Training')
+    end_time = time.time()
+    print(f'Finished Training, use {(end_time-start_time)/60} min')
 
 
 if __name__ == '__main__':
